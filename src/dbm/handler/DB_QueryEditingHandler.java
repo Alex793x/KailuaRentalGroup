3c698c527d7e4ba9db6623f133525662710c3060
Alexander Christensen <alex8970@stud.kea.dk>
2023-03-09 14:39:45 +0100

    added insertquery method

--- src/dbm/handler/DB_QueryEditingHandler.java
+++ src/dbm/handler/DB_QueryEditingHandler.java
@@ -1,5 +1,25 @@
 package dbm.handler;
 
+import dbm.DB_Dependencies;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DB_QueryEditingHandler {
 
+
+    public void insertQuery(String query) {
+        try (
+                java.sql.Connection connection = DriverManager.getConnection(
+                        DB_Dependencies.getInstance().database_url,
+                        DB_Dependencies.getInstance().username,
+                        DB_Dependencies.getInstance().password);
+                Statement s = connection.createStatement();
+        ) {
+            s.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
