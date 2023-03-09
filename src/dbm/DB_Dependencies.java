a069a707434635e8ccd97feac6eb73191dfc7c0e
Alexander Christensen <alex8970@stud.kea.dk>
2023-03-09 13:23:01 +0100

    first edition of printQuery

--- src/dbm/DB_Dependencies.java
+++ src/dbm/DB_Dependencies.java
@@ -1,4 +1,22 @@
 package dbm;
 
 public class DB_Dependencies {
+
+    private static DB_Dependencies theInstance = new DB_Dependencies();
+
+    // Login / Connection Fields ---------------------------------------------------------
+    public final String database_url = "sql7.freesqldatabase.com";
+    public final String username = "sql7603879";
+    public final String password = "kp1ScwPK48";
+
+    // Constructor -----------------------------------------------------------------------
+    private DB_Dependencies() {}
+
+    // Getter ----------------------------------------------------------------------------
+    public static DB_Dependencies getInstance() {
+        if (theInstance == null) {
+            theInstance = new DB_Dependencies();
+        }
+        return theInstance;
+    }
 }
