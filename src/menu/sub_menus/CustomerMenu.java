101cdb9dd69dfe3f09f54a0bbb07682e552422f4
Alexander Christensen <alex8970@stud.kea.dk>
2023-03-09 15:17:02 +0100

    Fixed column custommer array in db dependencies

--- src/menu/sub_menus/CustomerMenu.java
+++ src/menu/sub_menus/CustomerMenu.java
@@ -11,9 +11,6 @@ public class CustomerMenu extends Menu implements DBStandardQueries {
      * @param menuItems
      */
 
-    String[] allColumnNames = {"customer_id", "customer_name", "customer_address", "customer_zip",
-            "customer_city", "customer_phone", "customer_email", "customer_driver_license_number",
-            "customer_driver_since"};
     public CustomerMenu(String menuHeader, String[] menuItems) {
         super(menuHeader, menuItems);
     }
