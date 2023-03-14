c0fe705018d89ea7bf73312098eaa4f9cc965614
Alex Holmberg <alex793x@stud.kea.dk>
2023-03-14 11:58:53 +0100

    added further javaDoc

--- src/dbm/interfaces/query_interfaces/DBStandardQueries.java
+++ src/dbm/interfaces/query_interfaces/DBStandardQueries.java
@@ -16,14 +16,54 @@ import utility.UI;
 
 public interface DBStandardQueries {
 
+
+    /**
+     * <p>This method is part of the standard library of the DBStandardQueries. Specifically showing all data within a given table</p>
+     * @param requestHandler    Read javaDoc {@link DB_QueryRequestHandler}
+     * @param db_dependencies   Read javaDoc {@link DB_Dependencies}
+     */
     void showTable(DB_QueryRequestHandler requestHandler, DB_Dependencies db_dependencies);
 
+
+    /**
+     * <p>This method is part of the standard library of the DBStandardQueries. Specifically showing all data
+     * within a given table in ordered output. </p>
+     * @param requestHandler    Read javaDoc {@link DB_QueryRequestHandler}
+     * @param db_dependencies   Read javaDoc {@link DB_Dependencies}
+     */
     void showTableOrdered(DB_QueryRequestHandler requestHandler, DB_Dependencies db_dependencies);
 
+
+    /**
+     * <p>This method is part of the standard library of the DBStandardQueries. Specifically for inserting data within a given table</p>
+     * {@link UI#insertInto(String[] columnValues, DB_QueryRequestHandler requestHandler, String tableName, boolean, boolean)}
+     * @param editingHandler    Read javaDoc {@link DB_QueryEditingHandler}
+     * @param requestHandler    Read javaDoc {@link DB_QueryRequestHandler}
+     * @param ui                Read javaDoc {@link UI}
+     * @param db_dependencies   Read javaDoc {@link DB_Dependencies}
+     */
     void insertToTable(DB_QueryEditingHandler editingHandler, DB_QueryRequestHandler requestHandler, UI ui, DB_Dependencies db_dependencies);
 
+    /**
+     * <p>This method is part of the standard library of the DBStandardQueries. Specifically updating data within a given table. <br>
+     * This method is very similarly to {@link #insertToTable(DB_QueryEditingHandler, DB_QueryRequestHandler, UI, DB_Dependencies)},
+     * but has another functionality, which is to skip parameters, user isn't interested in changing.  </p>
+     *
+     * @param editingHandler    Read javaDoc {@link DB_QueryEditingHandler}
+     * @param requestHandler    Read javaDoc {@link DB_QueryRequestHandler}
+     * @param ui                Read javaDoc {@link UI}
+     * @param db_dependencies   Read javaDoc {@link DB_Dependencies}
+     */
     void updateTable(DB_QueryEditingHandler editingHandler,  DB_QueryRequestHandler requestHandler, UI ui, DB_Dependencies db_dependencies);
 
+
+    /**
+     *  <p>This method is part of the standard library of the DBStandardQueries. Specifically deleting data within a given table.</p>
+     * @param editingHandler    Read javaDoc {@link DB_QueryEditingHandler}
+     * @param requestHandler    Read javaDoc {@link DB_QueryRequestHandler}
+     * @param ui                Read javaDoc {@link UI}
+     * @param db_dependencies   Read JavaDoc {@link DB_Dependencies}
+     */
     void deleteFromTable(DB_QueryEditingHandler editingHandler,  DB_QueryRequestHandler requestHandler, UI ui, DB_Dependencies db_dependencies);
 
 }
